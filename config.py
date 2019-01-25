be7e5e1a4c16e576b7b736e7d79a02d1896b8c11
Vadim Nikolaev <defntvdm@gmail.com>
2019-01-26 00:51:36 +0500

    qutebrowser config init

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+    'g': 'https://www.google.com/search?q={}',
+    'y': 'https://yandex.ru/search/?text={}',
+    's': 'https://www.shodan.io/search?query={}'
+}
+
