422415d8cb567bcae3174c24952ceff71ead51e8
Vadim Nikolaev <defntvdm@gmail.com>
2021-08-01 16:44:12 +0500

    fixes

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -10,7 +10,6 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 4
 vim.o.smartindent = true
--- vim.o.t_Co = '256'
 vim.o.tabstop = 4
 vim.o.updatetime = 1000
 vim.o.swapfile = false
@@ -18,5 +17,6 @@ vim.o.ignorecase = true
 vim.o.exrc = true
 vim.o.secure = true
 vim.o.termguicolors = true
+vim.o.colorcolumn = '120'
 
 vim.g.python3_host_prog = '/home/defntvdm/.pyenv/shims/python3'
