9a29e68b6c5908817a139a4e1c84a258a22bbace
Vadim Nikolaev <defntvdm@yandex-team.ru>
2023-02-20 02:35:23 +0500

    rm noisy diagnostics

--- nvim/lua/user/null-ls.lua
+++ nvim/lua/user/null-ls.lua
@@ -5,34 +5,39 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	-- formatting
-	formatting.isort,
-	formatting.black,
-	formatting.djlint,
-	formatting.rustfmt,
-	formatting.goimports,
-	formatting.gofumpt,
-	formatting.clang_format,
-	formatting.prettierd,
-	formatting.stylua,
-	formatting.buf,
-	-- diagnostics
-	diagnostics.flake8,
-	diagnostics.mypy,
-	diagnostics.ruff,
-	diagnostics.pylint,
-	diagnostics.golangci_lint,
-	diagnostics.staticcheck,
-	diagnostics.revive,
-	diagnostics.cpplint,
-	diagnostics.eslint_d,
-	-- code_actions
-	code_actions.eslint_d,
-	code_actions.gitsigns,
-	code_actions.gomodifytags,
-	code_actions.ltrs,
+    -- formatting
+    formatting.isort,
+    formatting.black,
+    formatting.djlint,
+    formatting.rustfmt,
+    formatting.goimports,
+    formatting.gofumpt,
+    formatting.clang_format,
+    formatting.prettier.with({
+        extra_args = {
+            "--single-quote",
+            "--jsx-single-quote",
+            "--tab-width=2",
+            "--end-of-line=lf",
+            "--trailing-comma=all",
+            "--print-width=120",
+        },
+    }),
+    formatting.stylua,
+    formatting.buf,
+    -- diagnostics
+    diagnostics.flake8,
+    diagnostics.mypy,
+    diagnostics.ruff,
+    diagnostics.golangci_lint,
+    diagnostics.eslint_d,
+    -- code_actions
+    code_actions.eslint_d,
+    code_actions.gitsigns,
+    code_actions.gomodifytags,
+    code_actions.ltrs,
 }
 
 null_ls.setup({
-	sources = sources,
+    sources = sources,
 })
