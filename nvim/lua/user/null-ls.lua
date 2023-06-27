f50090e74d69e19102a32ffb712ba34a7ef28959
Vadim Nikolaev <defntvdm@gmail.com>
2023-06-27 22:41:48 +0500

    remove lines

--- nvim/lua/user/null-ls.lua
+++ nvim/lua/user/null-ls.lua
@@ -1,6 +1,5 @@
 local null_ls = require("null-ls")
 
-local completion = null_ls.builtins.completion
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
