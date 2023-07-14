09b3ad13f0bd79c8eb3aaa19a6f30c5c38e074ef
Vadim Nikolaev <defntvdm@gmail.com>
2023-07-14 14:58:19 +0500

    tokyonight theme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,11 +1,25 @@
 return {
-	"navarasu/onedark.nvim",
-	lazy = false,
-	priority = 1000,
+	"folke/tokyonight.nvim",
 	config = function()
-		require("onedark").setup({
-			style = "darker",
+		require("tokyonight").setup({
+			style = "night",
+			light_style = "day",
+			transparent = false,
+			terminal_colors = true,
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = {},
+				variables = {},
+				sidebars = "dark",
+				floats = "dark",
+			},
+			sidebars = { "qf", "help" },
+			day_brightness = 0.3,
+			hide_inactive_statusline = false,
+			dim_inactive = false,
+			lualine_bold = false,
 		})
-		require("onedark").load()
+		vim.cmd.colorscheme("tokyonight")
 	end,
 }
