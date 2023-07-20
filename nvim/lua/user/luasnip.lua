478c651504aa02e51cadfbecc23bfaf8a4ccd169
Vadim Nikolaev <defntvdm@gmail.com>
2023-07-20 17:50:46 +0500

    call signature by keymap

--- nvim/lua/user/luasnip.lua
+++ nvim/lua/user/luasnip.lua
@@ -12,10 +12,8 @@ return {
 		"rafamadriz/friendly-snippets",
 	},
 	keys = {
-		{ "i", "<c-j>", next, silent = true, noremap = true },
-		{ "i", "<c-k>", prev, silent = true, noremap = true },
-		{ "s", "<c-j>", next, silent = true, noremap = true },
-		{ "s", "<c-k>", prev, silent = true, noremap = true },
+		{ "<c-j>", next, mode = { "i", "s" }, silent = true, noremap = true },
+		{ "<c-k>", prev, mode = { "i", "s" }, silent = true, noremap = true },
 	},
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
