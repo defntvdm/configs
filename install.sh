8cb606197a0c749927a37362e125817e1b636e90
Vadim Nikolaev <defntvdm@yandex-team.ru>
2024-02-13 17:30:39 +0500

    actualize install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-cp ./alacritty.yml ~/.alacritty.yml
+cp ./alacritty.toml ~/.alacritty.toml
+cp ./clang-format ~/.clang-format
 mkdir -p ~/.config
 cp ./gitconfig ~/.gitconfig
 cp ./gitignore_global ~/.gitignore_global
-mkdir -p ~/.config/i3
-cp ./i3.config ~/.config/i3/config
-mkdir -p ~/.config/i3status
-cp ./i3status.config ~/.config/i3status/config
 mkdir -p ~/.oh-my-zsh/themes
 cp ./mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
 cp ./tmux.conf ~/.tmux.conf
 cp -r nvim ~/.config/nvim
 cp ./zshrc ~/.zshrc
-cp ./flake8 ~/.config/
+cp ./ruff.toml ~/.ruff.toml
