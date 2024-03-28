f7193e62e7c63935d72cb9e89df8dbe46300b6cb
Vadim Nikolaev <defntvdm@yandex-team.ru>
2024-03-28 15:20:35 +0500

    more config in wezterm, add clang-format rules

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,16 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
+config.font_rules = {
+	{
+		intensity = "Bold",
+		font = wezterm.font("JetBrainsMono Nerd Font", {
+			weight = "Bold",
+			style = "Normal",
+		}),
+	},
+}
+
 config.keys = {
 	{
 		key = "LeftArrow",
@@ -14,6 +24,13 @@ config.keys = {
 		mods = "OPT",
 		action = wezterm.action.SendString("\x1bf"),
 	},
+}
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
