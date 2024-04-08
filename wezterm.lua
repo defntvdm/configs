029e78064789cbdfb5454ead22a56df08846a202
Vadim Nikolaev <defntvdm@yandex-team.ru>
2024-04-09 00:55:32 +0500

    italic bold font, renaming tab in wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
@@ -6,14 +7,29 @@ config.font_size = 14.0
 config.font_rules = {
 	{
 		intensity = "Bold",
+		italic = false,
 		font = wezterm.font("JetBrainsMono Nerd Font", {
 			weight = "Bold",
+			italic = false,
 			style = "Normal",
+		}),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrainsMono Nerd Font", {
+			weight = "Bold",
+			italic = true,
 		}),
 	},
 }
 
 config.keys = {
+	{
+		key = "m",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
 	{
 		key = "LeftArrow",
 		mods = "OPT",
@@ -23,6 +39,18 @@ config.keys = {
 		key = "RightArrow",
 		mods = "OPT",
 		action = wezterm.action.SendString("\x1bf"),
+	},
+	{
+		key = "r",
+		mods = "CMD",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
 	},
 }
 config.window_decorations = "RESIZE"
