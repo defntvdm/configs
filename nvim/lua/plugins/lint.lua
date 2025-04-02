b73884097da5ce7ee5bc6a671205d6daa8bef2b7
Vadim Nikolaev <defntvdm@gmail.com>
2025-04-02 22:24:43 +0500

    rm ruff

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,40 +1,50 @@
 local function lint_file()
-	require("lint").try_lint()
+    require("lint").try_lint()
 end
 
 return {
-	"mfussenegger/nvim-lint",
-	keys = {
-		{ "<leader>l", lint_file, desc = "Trigger linting for current file" },
-	},
-	ft = defntvdm_filetypes,
-	config = function()
-		local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    keys = {
+        { "<leader>l", lint_file, desc = "Trigger linting for current file" },
+    },
+    ft = defntvdm_filetypes,
+    config = function()
+        local lint = require("lint")
 
-		lint.linters_by_ft = {
-			c = {},
-			cmake = { "cmakelint" },
-			cpp = {},
-			css = { "eslint_d" },
-			go = { "golangcilint" },
-			html = { "eslint_d" },
-			javascript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			json = { "jsonlint" },
-			less = { "eslint_d" },
-			markdown = { "markdownlint" },
-			proto = { "buf_lint" },
-			python = { "ruff", "mypy" },
-			typescript = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			yaml = { "yamllint" },
-		}
+        lint.linters.golangcilint.args = {
+            'run',
+            '--output.json.path=stdout',
+            '--issues-exit-code=0',
+            '--show-stats=false',
+            function()
+                return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+            end
+        }
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        lint.linters_by_ft = {
+            c = {},
+            cmake = { "cmakelint" },
+            cpp = {},
+            css = { "eslint_d" },
+            go = { "golangcilint" },
+            html = { "eslint_d" },
+            javascript = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            json = { "jsonlint" },
+            less = { "eslint_d" },
+            markdown = { "markdownlint" },
+            proto = { "buf_lint" },
+            python = { "mypy" },
+            typescript = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            yaml = { "yamllint" },
+        }
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = lint_file,
-		})
-	end,
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            group = lint_augroup,
+            callback = lint_file,
+        })
+    end,
 }
--- nvim/presets/python.lua
+++ nvim/presets/python.lua
@@ -2,73 +2,74 @@ local lsp = require("lspconfig")
 
 local pythonPath = "/home/defntvdm/.pyenv/versions/<VERSION>/bin/python"
 
-lsp.pyright.setup({
-	cmd = { "pyright-langserver", "--stdio" },
-	settings = {
-		python = {
-			pythonPath = pythonPath,
-		},
-		pyright = {
-			analysis = {
-				autoSearchPaths = true,
-				diagnosticMode = "workspace",
-				useLibraryCodeForTypes = true,
-			},
-		},
-	},
-	on_attach = custom_attach,
-	capabilities = custom_capabilities,
+lsp.basedpyright.setup({
+    settings = {
+        python = {
+            pythonPath = pythonPath,
+        },
+        basedpyright = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true,
+                ignore = { '*' },
+                typeCheckingMode = "off",
+            },
+        },
+    },
+    on_attach = custom_attach,
+    capabilities = custom_capabilities,
 })
 
 local dap = require("dap")
 
 dap.adapters.python = function(cb, config)
-	if config.request == "attach" then
-		local port = (config.connect or config).port
-		local host = (config.connect or config).host or "127.0.0.1"
-		cb({
-			type = "server",
-			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
-			host = host,
-			options = {
-				source_filetype = "python",
-			},
-		})
-	else
-		cb({
-			type = "executable",
-			command = pythonPath,
-			args = { "-m", "debugpy.adapter" },
-			options = {
-				source_filetype = "python",
-			},
-		})
-	end
+    if config.request == "attach" then
+        local port = (config.connect or config).port
+        local host = (config.connect or config).host or "127.0.0.1"
+        cb({
+            type = "server",
+            port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+            host = host,
+            options = {
+                source_filetype = "python",
+            },
+        })
+    else
+        cb({
+            type = "executable",
+            command = pythonPath,
+            args = { "-m", "debugpy.adapter" },
+            options = {
+                source_filetype = "python",
+            },
+        })
+    end
 end
 
 dap.adapters.django = {
-	type = "server",
-	id = "debugpy",
-	port = "${port}",
-	executable = {
-		command = pythonPath,
-		args = { "-m", "debugpy", "--listen", "${port}", "--wait-for-client", "manage.py", "runserver" },
-	},
+    type = "server",
+    id = "debugpy",
+    port = "${port}",
+    executable = {
+        command = pythonPath,
+        args = { "-m", "debugpy", "--listen", "${port}", "--wait-for-client", "manage.py", "runserver" },
+    },
 }
 
 dap.configurations.python = {
-	{
-		type = "python",
-		request = "launch",
-		name = "Launch file",
-		program = "${file}",
-		pythonPath = pythonPath,
-	},
-	{
-		type = "django",
-		request = "attach",
-		name = "Django",
-		-- program = vim.fn.getcwd() .. "/manage.py",
-		-- args = { "runserver" },
-	},
+    {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = pythonPath,
+    },
+    {
+        type = "django",
+        request = "attach",
+        name = "Django",
+        -- program = vim.fn.getcwd() .. "/manage.py",
+        -- args = { "runserver" },
+    },
 }
