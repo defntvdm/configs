404fa147465c6ed41829b6785c11bad6b4edb88b
Vadim Nikolaev <defntvdm@gmail.com>
2025-06-05 19:57:18 +0500

    add yazi plugin, use yabro as default again

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,29 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"folke/snacks.nvim",
+	},
+	keys = {
+		{
+			"-",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			" -",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+	},
+	opts = {
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	init = function()
+		vim.g.loaded_netrwPlugin = 1
+	end,
+}
