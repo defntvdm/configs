2e1592a9eba0e365ba258c8daf177b8a21e7ca6e
Vadim Nikolaev <defntvdm@gmail.com>
2025-10-27 00:56:32 +0500

    fix bufferline catppuccin

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,33 +1,33 @@
 return {
-	"akinsho/bufferline.nvim",
-	enabled = not vim.g.vscode,
-	dependencies = "nvim-tree/nvim-web-devicons",
-	event = "VimEnter",
-	config = function()
-		local palettes = require("catppuccin.palettes").get_palette("macchiato")
-		require("bufferline").setup({
-			highlights = require("catppuccin.groups.integrations.bufferline").get_theme({
-				styles = { "italic", "bold" },
-				custom = {
-					all = {
-						fill = { bg = palettes.base },
-					},
-				},
-			}),
-			options = {
-				mode = "tabs",
-				themable = true,
-				numbers = "buffer_id",
-				indicator = {
-					style = "underline",
-				},
-				buffer_close_icon = "󰅖",
-				modified_icon = "●",
-				close_icon = "",
-				left_trunc_marker = "",
-				right_trunc_marker = "",
-				diagnostics = "nvim_lsp",
-			},
-		})
-	end,
+    "akinsho/bufferline.nvim",
+    enabled = not vim.g.vscode,
+    dependencies = "nvim-tree/nvim-web-devicons",
+    event = "VimEnter",
+    config = function()
+        local palettes = require("catppuccin.palettes").get_palette("macchiato")
+        require("bufferline").setup({
+            highlights = require("catppuccin.special.bufferline").get_theme({
+                styles = { "italic", "bold" },
+                custom = {
+                    all = {
+                        fill = { bg = palettes.base },
+                    },
+                },
+            }),
+            options = {
+                mode = "tabs",
+                themable = true,
+                numbers = "buffer_id",
+                indicator = {
+                    style = "underline",
+                },
+                buffer_close_icon = "󰅖",
+                modified_icon = "●",
+                close_icon = "",
+                left_trunc_marker = "",
+                right_trunc_marker = "",
+                diagnostics = "nvim_lsp",
+            },
+        })
+    end,
 }
